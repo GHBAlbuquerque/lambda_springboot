36f5ba99e20b4a54696d74717569fa77ddb651e7
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-09-18 20:27:44 -0300

    commit inicial

---
+++ app/src/main/java/br/com/ghbalbuquerque/lambdaspringboot/LambdaspringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.ghbalbuquerque.lambdaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaspringbootApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/com/ghbalbuquerque/lambdaspringboot/LambdaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ghbalbuquerque.lambdaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
